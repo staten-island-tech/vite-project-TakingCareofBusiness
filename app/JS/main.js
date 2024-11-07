b120477bcef5f4030b9b82d46d922f8f3aea39eb
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-11-07 12:28:32 -0500

    Izzy touched me

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,13 +1,17 @@
 import "../CSS/style.css";
 import { albums } from "./array.js";
-function cardAdder() {
-  albums.forEach((album) =>
+function cardAdder(array) {
+  array.forEach((album) =>
     document
       .querySelector(".card-container")
       .insertAdjacentHTML(
         "beforeEnd",
-        `<div class="card"><h2 class="card-header">${album.name}</h2><img src=${album.cover} class="card-image"><p class="card-artist">${album.artist}</p><p class="card-genre">${album.genre}</p><p class="card-date">${album.releaseDate}</p><p class="card-length">${album.albumLength}</p><p class="card-amount">${album.albumAmount}</p></div>`
+        `<div class="card"><div class="header-container"><h2>${album.name}</h2></div><img src=${album.cover} class="card-image"><div class="info-container"><p class="card-artist">${album.artist}</p><p class="card-genre">Genre: ${album.genre}</p><p class="card-date"> Date Released: ${album.releaseDate}</p><p class="card-length"> Album Length: ${album.albumLength} minutes</p><p class="card-amount">Number of Songs: ${album.albumAmount}</p></div></div>`
       )
   );
 }
-cardAdder();
+function createSorted() {
+  document.querySelector(".card-container").innerHTML = "";
+  albums.filter;
+}
+cardAdder(albums);
